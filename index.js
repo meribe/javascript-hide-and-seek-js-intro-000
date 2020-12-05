1d99d9a16e7a5dbb90edd4e187a0aa29c79e2ef5
witty-abstraction-1855 <merve.kastan4@gmail.com>
2020-12-05 18:45:18 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
+function getFirstSelector(selector) {
+  const a= document.querySelector(selector)
+  return a
+}
 
+function nestedTarget(){
+  const d= document
+  .getElementById("nested").querySelector("div.target")
+  return d
+}
+
+function increaseRankBy(n){
+  const lis = document
+  .querySelectorAll('.ranked-list');
+
+        for (let i = 0; i < lis.length; i++) {
+      lis[i].innerHTML = ((parseInt(lis[i].innerHTML)) + n)
+    }
+
+}
+
+function deepestChild(){
+  var deep = document.getElementById('grand-node').querySelectorAll('div')
+  return deep[deep.length-1]
+
+}
